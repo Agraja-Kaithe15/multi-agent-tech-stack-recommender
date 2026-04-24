cf8cdb3da1370c0a9dfba9ee8fb81dc7d61999ae
Agraja Kaithe <vijaykaithe@gmail.com>
2026-04-24 22:03:49 +0530

    update gemini model name

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-1.5-flash")
+GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-1.5-flash-latest")
